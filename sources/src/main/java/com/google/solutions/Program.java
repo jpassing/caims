b4effb4e85a82ac1e1ccda1efa13fe57c19fda86
Johannes Passing <jpassing@hotmail.com>
2025-06-05 08:50:57 +0200

    Parse command line

---
+++ sources/src/main/java/com/google/solutions/Program.java
@@ -0,0 +1,33 @@
+package com.google.solutions;
+
+import java.util.Arrays;
+
+public class Program {
+  /**
+   * Entry point, typically invoked using maven or by running
+   * <c>java -jar</c>.
+   */
+  public static void main(String[] args) {
+    var action = Arrays.stream(args).findFirst().orElse("");
+    switch (action) {
+      case "client":
+        System.out.println("Running as client");
+        return;
+
+      case "broker":
+        System.out.println("Running as broker");
+        return;
+
+      case "server":
+        System.out.println("Running as server");
+        return;
+
+      default:
+        System.err.printf("Unrecognized action '%s'\n", action);
+        System.err.println("Supported actions are:");
+        System.err.println("  client:    Run client application");
+        System.err.println("  broker:    Run broker (typically run in Cloud Run)");
+        System.err.println("  server:    Run server (typically run on a confidential VM)");
+    }
+  }
+}
