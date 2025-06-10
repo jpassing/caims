2562084258cf267730eab0196032aa9b5c7ad00d
Johannes Passing <jpassing@hotmail.com>
2025-06-10 09:25:51 +0200

    Use Google HttpClient

--- sources/src/main/java/com/google/solutions/caims/workload/MetadataServer.java
+++ sources/src/main/java/com/google/solutions/caims/workload/MetadataServer.java
@@ -1,5 +1,11 @@
 package com.google.solutions.caims.workload;
 
+import com.google.api.client.http.ByteArrayContent;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonObjectParser;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.GenericData;
 import com.google.auth.oauth2.ComputeEngineCredentials;
 import com.google.common.base.Preconditions;
 import org.jetbrains.annotations.NotNull;
@@ -11,6 +17,7 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Helper class for interacting with the Compute Engine
@@ -32,31 +39,57 @@ public class MetadataServer {
     Preconditions.checkArgument(!namespace.contains("/"), "Namespace must not contain slashes");
     Preconditions.checkArgument(!name.contains("/"), "Name must not contain slashes");
 
+    var url = new GenericUrl(String.format(
+      "%s/computeMetadata/v1/instance/guest-attributes/%s/%s",
+      ComputeEngineCredentials.getMetadataServerUrl(),
+      namespace,
+      name));
+
+    var request = new NetHttpTransport()
+      .createRequestFactory()
+      .buildPutRequest(
+        url,
+        new ByteArrayContent("text/plain", value.getBytes(StandardCharsets.UTF_8)));
+
+    request.setParser(new JsonObjectParser(GsonFactory.getDefaultInstance()));
+    request.getHeaders().set("Metadata-Flavor", "Google");
+    request.setThrowExceptionOnExecuteError(true);
+
     try {
-      var request = HttpRequest
-        .newBuilder()
-        .uri(new URI(
-          String.format(
-            "%s/computeMetadata/v1/instance/guest-attributes/%s/%s",
-            ComputeEngineCredentials.getMetadataServerUrl(),
-            namespace,
-            name)))
-        .header("Metadata-Flavor", "Google")
-        .PUT(HttpRequest.BodyPublishers.ofString(value))
-        .build();
-
-      var response = this.client
-        .send(request, HttpResponse.BodyHandlers.ofString());
-
-      if (response.statusCode() != HttpURLConnection.HTTP_OK) {
-        throw new IOException(
-          String.format(
-            "Setting guest attribute failed with HTTP error code: %d",
-            response.statusCode()));
-      }
+      request.execute();
     }
-    catch (URISyntaxException | InterruptedException e) {
-      throw new IOException(e);
+    catch (Exception exception) {
+      throw new IOException(
+        "Setting guest attribute failed",
+        exception);
+    }
+  }
+
+  /**
+   * Get instance metadata for the current VM.
+   */
+  public @NotNull GenericData getInstanceMetadata() throws IOException {
+    var url = new GenericUrl(
+      ComputeEngineCredentials.getMetadataServerUrl() +
+        "/computeMetadata/v1/project/?recursive=true");
+
+    var request = new NetHttpTransport()
+      .createRequestFactory()
+      .buildGetRequest(url);
+
+    request.setParser(new JsonObjectParser(GsonFactory.getDefaultInstance()));
+    request.getHeaders().set("Metadata-Flavor", "Google");
+    request.setThrowExceptionOnExecuteError(true);
+
+    try {
+      return request
+        .execute()
+        .parseAs(GenericData.class);
+    }
+    catch (Exception exception) {
+      throw new IOException(
+        "Cannot find the metadata server, possibly because code is not running on Google Cloud",
+        exception);
     }
   }
 }
